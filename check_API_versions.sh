55726a71f7cfb091ac9ec1218c22bf67c6ad891e
Morgan Heimbeck <morganryerheimbeck@gmail.com>
2018-08-03 10:57:23 -0700

    Update directory, moved from log to logs

--- check_API_versions.sh
+++ check_API_versions.sh
@@ -8,7 +8,7 @@ cd /spec/Syrinx.ApiEdge/ && git s && git --no-pager lg -4 &&
 printf '\n!!!  release !!!\n' &&
 cd /spec/Syrinx.releaseCandidate/ && git s && git --no-pager lg -4 &&
 printf '\nPending SQL files\n' && find . -name '*.sql' -exec rcs.pending "{}" \; | fgrep -v 'No unde' &&
-printf '\n\n maybe not checked back in or released' && cd /log/rcs/pending/ && fgrep '.sql' * | fgrep -v 'remove' &&
+printf '\n\n maybe not checked back in or released' && cd /logs/rcs/pending/ && fgrep '.sql' * | fgrep -v 'remove' &&
 exit"
 
 ssh morgan@sys5a.tradetech.net "printf '\n!!!!!! SYS5A !!!!!!\n' &&
